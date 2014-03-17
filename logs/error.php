e44b9024558955fac929c2403f52ef08dfbe929a
Simon Asika <asika32764@gmail.com>
2014-03-17 16:44:56 +0800

    First Commit

---
+++ logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-03-17 08:44:12 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2014-03-17T08:44:12+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
