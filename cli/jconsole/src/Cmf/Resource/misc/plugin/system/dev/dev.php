95937061ed176f0017216408af749e4cc21d362b
Simon Asika <asika32764@gmail.com>
2016-03-06 18:30:31 +0800

    Rename dev plugin

--- cli/jconsole/src/Cmf/Resource/misc/plugin/system/dev/dev.php
+++ cli/jconsole/src/Cmf/Resource/misc/plugin/system/dev/dev.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Part of SMS CMF project.
+ * Part of CMF project.
  *
- * @copyright  Copyright (C) 2011 - 2014 SMS Taiwan, Inc. All rights reserved.
+ * @copyright  Copyright (C) 2016 LYRASOFT. All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE
  */
 
